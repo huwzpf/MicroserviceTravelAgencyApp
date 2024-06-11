627d720654857c5285fb3cebf1028f990cb7930b
maj0r <sebastian.lesniewski.edu@gmail.com>
2024-06-11 10:00:47 +0200

    Add IP choose when running script - ex: python .\main.py http://localhost:18460 or python .\main.py for default ip and port.

--- discount_script/main.py
+++ discount_script/main.py
@@ -2,30 +2,32 @@ import requests
 import random
 import time
 import schedule
+import argparse
 
-def login():
-    url = 'http://localhost:18460/api/Auth/Login/'
+
+def login(ip):
+    url = f'{ip}/api/Auth/Login/'
     payload = {"username": "admin", "password": "passwordadmin"}
     response = requests.post(url, json=payload)
     response.raise_for_status()
     return response.json()["token"]
 
-def get_tours():
-    url = f'http://localhost:18460/api/Tours/'
+def get_tours(ip):
+    url = f'{ip}/api/Tours/'
     params = {'minDuration': 7, 'maxDuration': 9}
     response = requests.get(url, params=params)
     response.raise_for_status()
     return response.json()
 
-def get_hotels(token):
-    url = 'http://localhost:18460/api/Hotels'
+def get_hotels(token,ip):
+    url = f'{ip}/api/Hotels'
     headers = {'Authorization': f'{token}'}
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.json()
 
-def make_reservation(token, tours_data):
-    url = f'http://localhost:18460/api/Reservations/'
+def make_reservation(token, tours_data,ip):
+    url = f'{ip}/api/Reservations/'
     headers = {'Authorization': f'{token}'}
 
     for tour in tours_data:
@@ -50,8 +52,8 @@ def save_first_hotel(hotels):
     else:
         return None
     
-def get_transport_options(token):
-    url = 'http://localhost:18460/api/TransportOptions'
+def get_transport_options(token,ip):
+    url = f'{ip}/api/TransportOptions'
     headers = {'Authorization': f'{token}'}
     response = requests.get(url, headers=headers)
     response.raise_for_status()
@@ -63,44 +65,58 @@ def save_first_transport_option(transport_options):
     else:
         return None
 
-def post_discount_hotel(token, hotel_id):
+def post_discount_hotel(token, hotel_id,ip):
     headers = {'Authorization': f'{token}'}
     random_value = round(random.random(),2)
     payload = {"id": hotel_id, "value": random_value}
-    url = f'http://localhost:18460/api/Hotels/{hotel_id}/Discount'
+    url = f'{ip}/api/Hotels/{hotel_id}/Discount'
     response = requests.post(url, json=payload, headers=headers)
     response.raise_for_status()
     print(f"Posted discount for hotel {hotel_id} with value {random_value}")
 
-def post_discount_transport(token, transport_option_id):
+def post_discount_transport(token, transport_option_id,ip):
     headers = {'Authorization': f'{token}'}
     random_value = round(random.random(),2)
     payload = {"id": transport_option_id, "value": random_value}
-    url = f'http://localhost:18460/api/TransportOptions/{transport_option_id}/Discount'
+    url = f'{ip}/api/TransportOptions/{transport_option_id}/Discount'
     response = requests.post(url, json=payload, headers=headers)
     response.raise_for_status()
     print(f"Posted discount for transport option {transport_option_id} with value {random_value}")
 
 def main():
-    token = login()
+    parser = argparse.ArgumentParser(description='Run generator on specific IP.')
 
-    tours_data = get_tours()
+    parser.add_argument('ip', type=str, help='IP with port.')
 
-    make_reservation(token, tours_data)
+    args = parser.parse_args()
 
-    hotels = get_hotels(token)
+    if args.ip:
+        ip = args.ip
+    else:
+        ip = "http://localhost:18460"
+
+    print(f'IP: {ip}')
+
+
+    token = login(ip)
+
+    tours_data = get_tours(ip)
+
+    make_reservation(token, tours_data,ip)
+
+    hotels = get_hotels(token,ip)
     first_hotel = save_first_hotel(hotels)
     if first_hotel:
         print("First hotel saved:", first_hotel)
-        schedule.every(5).seconds.do(post_discount_hotel, token, first_hotel['id'])
+        schedule.every(5).seconds.do(post_discount_hotel, token, first_hotel['id'],ip)
     else:
         print("No hotels found.")
 
-    transport_options = get_transport_options(token)
+    transport_options = get_transport_options(token,ip)
     first_transport_option = save_first_transport_option(transport_options)
     if first_transport_option:
         print("First transport option saved:", first_transport_option)
-        schedule.every(7).seconds.do(post_discount_transport, token, first_transport_option['id'])
+        schedule.every(7).seconds.do(post_discount_transport, token, first_transport_option['id'],ip)
     else:
         print("No transport options found.")
 
